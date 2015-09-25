3fee5fd7a9dfcf84cb62f6d1e3add7a29ba62d31
Dennis Wilkins <wilkinsd@nuvi.com>
2015-09-25 09:40:55 -0600

    better routes

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 import React from "react";
-import { Router, Route } from 'react-router';
+import { Router, Route, Link } from 'react-router';
 import SimpleD3 from "./views/simple_d3";
 import AllReact from "./views/all_react";
 import Advanced from "./views/advanced";
 
+class Layout extends React.Component {
+  render () {
+    return (
+      <div className="layout">
+        <ul>
+          <li><Link to="/simple">Simple</Link></li>
+          <li><Link to="/react">All React</Link></li>
+          <li><Link to="/advanced">Advanced</Link></li>
+        </ul>
+        <div className="container">
+          { this.props.children }
+        </div>
+      </div>
+    );
+  }
+}
 
-React.render((
-  <Router>
-    <Route path="/" component={SimpleD3} />
-    <Route path="react" component={AllReact} />
-    <Route path="advanced" component={Advanced} />
-  </Router>), document.getElementById("react"));
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <Route path="/" component={Layout}>
+          <Route path="simple" component={SimpleD3} />
+          <Route path="react" component={AllReact} />
+          <Route path="advanced" component={Advanced} />
+        </Route>
+      </Router>
+    );
+  }
+};
+App.contextTypes = {
+  router: React.PropTypes.func
+};
+
+React.render(<App />, document.getElementById("react"));
